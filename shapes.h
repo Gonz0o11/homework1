643b9d5431e45cfd3bb9c2ea605030ba5e2f6bfb
Gonz0o11 <Gonz0o101010@gmail.com>
2023-02-28 17:18:53 +0800

    Finish

--- shapes.h
+++ shapes.h
@@ -4,17 +4,22 @@
 #define PI 3.1415926
 
 // Base class
-class Shape {
+class Shape
+{
 public:
-	Shape(double a = 0, double b = 0) {
-		width = a; height = b;
+	Shape(double a = 0, double b = 0)
+	{
+		width = a;
+		height = b;
 	}
 	~Shape() {}
 
-	void setWidth(double w) {
+	void setWidth(double w)
+	{
 		width = w;
 	}
-	void setHeight(double h) {
+	void setHeight(double h)
+	{
 		height = h;
 	}
 	virtual double area() = 0;
@@ -24,12 +29,36 @@ protected:
 	double height;
 };
 
-class Rectangle : public Shape {
+class Rectangle : public Shape
+{
 public:
-	Rectangle(double a = 0, double b = 0) :Shape(a, b) { }
+	Rectangle(double a = 0, double b = 0) : Shape(a, b) {}
 
-	double area() {
+	double area()
+	{
 		return (width * height);
+	}
+};
+
+class Triangle : public Shape
+{
+public:
+	Triangle(double a = 0, double b = 0) : Shape(a, b) {}
+
+	double area()
+	{
+		return (width * height / 2);
+	}
+};
+
+class Circle : public Shape
+{
+public:
+	Circle(double a = 0) : Shape(a) {}
+
+	double area()
+	{
+		return (width * width * PI);
 	}
 };
 
